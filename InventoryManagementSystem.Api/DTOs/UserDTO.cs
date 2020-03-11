60781fdf1574a7587ec94347f297b3f89907970d
Mohamad Fayad <mhmdfayad199@gmail.com>
2020-03-11 06:22:40 -0700

    add userdto

---
+++ InventoryManagementSystem.Api/DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+namespace InventoryManagementSystem.Api.DTOs
+{
+    public class UserDTO
+    {
+        public long Id {get; set;}
+        public string Username {get; set;}
+        public string Password {get; set;}
+    }
+}
