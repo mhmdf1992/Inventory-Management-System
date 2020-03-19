a3a3db19244a8ebe6ed3d1f09b98674b77365344
Mohamad Fayad <mhmdfayad199@gmail.com>
2020-03-19 20:45:25 +0200

    implement validation on registerform

--- InventoryManagementSystem.Spa/ims/src/Components/Auth/Register/Form/RegisterForm.js
+++ InventoryManagementSystem.Spa/ims/src/Components/Auth/Register/Form/RegisterForm.js
@@ -1,54 +1,82 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 import './RegisterForm.css';
 
-class RegisterForm extends Component{
-    constructor(props){
-        super(props);
-        this.state = this.props.data;
-        this.state.confirm = '';
-    }
-
-    render(){
-        return (
-            <div className="register-form">
-                <form>
-                    <div className="row">
-                        <input type="text" className="form-input full-width" placeholder="Firstname"
-                            value={this.state.firstname}
-                            onChange={(e) => this.setState({ firstname: e.target.value })} />
-                    </div>
-                    <div className="row">
-                        <input type="text" className="form-input full-width" placeholder="Lastname"
-                            value={this.state.lastname}
-                            onChange={(e) => this.setState({ lastname: e.target.value })} />
-                    </div>
-                    <div className="row">
-                        <input type="text" className="form-input full-width" placeholder="Email"
-                            value={this.state.email}
-                            onChange={(e) => this.setState({ email: e.target.value })} />
-                    </div>
-                    <div className="row">
-                        <input type="password" className="form-input full-width" placeholder="Password"
-                            value={this.state.password}
-                            onChange={(e) => this.setState({ password: e.target.value })} />
-                    </div>
-                    <div className="row">
-                        <input type="password" className="form-input full-width" placeholder="Confirm"
-                            value={this.state.confirm}
-                            onChange={(e) => this.setState({ confirm: e.target.value })} />
-                    </div>
-                    <div className="row">
-                        <label className="err-msg">{this.state.password !== this.state.confirm ? 'Confirm password does not match' : ''}</label>
-                    </div>
-                    <div className="footer">
-                        <button className="register"
-                            onClick={e =>{  e.preventDefault(); this.props.onAction(this.state);}}>Register</button>
-                    </div>
-                </form>
-            </div>
-        )
-    }
-}
-
+const RegisterForm = (props) => {
+    return (
+        <Formik
+            initialValues={{ ...props.data, confirm: '' }}
+            validationSchema={Yup.object({
+                firstname: Yup.string()
+                    .required('Required')
+                    .min(3, 'Minimum 3 characters long')
+                    .max(25, 'Maximum 25 characters long'),
+                lastname: Yup.string()
+                    .required('Required')
+                    .min(3, 'Minimum 3 characters long')
+                    .max(25, 'Maximum 25 characters long'),
+                email: Yup.string()
+                    .email('Invalid email address')
+                    .required('Required'),
+                password: Yup.string()
+                    .required('Required')
+                    .min(6)
+                    .max(25, 'Maximum 25 characters long'),
+                confirm: Yup.string()
+                    .required('Required')
+                    .oneOf([Yup.ref("password")], "Password missmatch")
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+                props.onAction(values);
+            }}
+        >
+            {formik => (
+                <div className="register-form">
+                    <form onSubmit={formik.handleSubmit}>
+                        <div className="row">
+                            <input id="firstname" className="form-input full-width" placeholder="Firstname"
+                                {...formik.getFieldProps('firstname')} />
+                            {formik.touched.firstname && formik.errors.firstname ? (
+                                <span className="err-msg">{formik.errors.firstname}</span>
+                            ) : null}
+                        </div>
+                        <div className="row">
+                            <input id="lastname" className="form-input full-width" placeholder="Lastname"
+                                {...formik.getFieldProps('lastname')} />
+                            {formik.touched.lastname && formik.errors.lastname ? (
+                                <span className="err-msg">{formik.errors.lastname}</span>
+                            ) : null}
+                        </div>
+                        <div className="row">
+                            <input id="email" className="form-input full-width" placeholder="Email"
+                                {...formik.getFieldProps('email')} />
+                            {formik.touched.email && formik.errors.email ? (
+                                <span className="err-msg">{formik.errors.email}</span>
+                            ) : null}
+                        </div>
+                        <div className="row">
+                            <input id="password" type="password" className="form-input full-width" placeholder="Password"
+                                {...formik.getFieldProps('password')} />
+                            {formik.touched.password && formik.errors.password ? (
+                                <span className="err-msg">{formik.errors.password}</span>
+                            ) : null}
+                        </div>
+                        <div className="row">
+                            <input id="confirm" type="password" className="form-input full-width" placeholder="Confirm Password"
+                                {...formik.getFieldProps('confirm')} />
+                            {formik.touched.confirm && formik.errors.confirm ? (
+                                <span className="err-msg">{formik.errors.confirm}</span>
+                            ) : null}
+                        </div>
+                        <div className="footer">
+                            <button type="submit" className="register">Register</button>
+                        </div>
+                    </form>
+                </div>
+            )}
+        </Formik>
+    );
+};
 export default RegisterForm;
