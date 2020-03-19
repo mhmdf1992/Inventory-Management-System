063ede0ad9fa43a512e2f3f44d780017b1933eb1
Mohamad Fayad <mhmdfayad199@gmail.com>
2020-03-19 18:05:44 +0200

    implement validation on loginform

--- InventoryManagementSystem.Spa/ims/src/Components/Auth/Login/Form/LoginForm.js
+++ InventoryManagementSystem.Spa/ims/src/Components/Auth/Login/Form/LoginForm.js
@@ -1,38 +1,49 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 import './LoginForm.css';
 
-class LoginForm extends Component{
-    constructor(props){
-        super(props);
-        this.state = this.props.data;
-    }
+const LoginForm = (props) => {
+    return (
+        <Formik
+            initialValues={{ ...props.data }}
+            validationSchema={Yup.object({
+                email: Yup.string()
+                    .email('Invalid email address')
+                    .required('Required'),
+                password: Yup.string()
+                    .required('Required')
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+                props.onAction(values);
+            }}
+        >
+            {formik => (
+                <div className="login-form">
+                    <form onSubmit={formik.handleSubmit}>
+                        <div className="row">
+                            <input id="email" className="form-input full-width" placeholder="Email"
+                                {...formik.getFieldProps('email')} />
+                            {formik.touched.email && formik.errors.email ? (
+                                <span className="err-msg">{formik.errors.email}</span>
+                            ) : null}
+                        </div>
+                        <div className="row">
+                            <input id="password" type="password" className="form-input full-width" placeholder="Password"
+                                {...formik.getFieldProps('password')} />
+                            {formik.touched.password && formik.errors.password ? (
+                                <span className="err-msg">{formik.errors.password}</span>
+                            ) : null}
+                        </div>
+                        <div className="footer">
+                            <button type="submit" className="login">Login</button>
+                        </div>
 
-    render(){
-        return (
-            <div className="login-form">
-                <form>
-                    <div className="row">
-                        <input type="text" className="form-input full-width" placeholder="Email"
-                            value={this.state.email}
-                            onChange={(e) => this.setState({ email: e.target.value })} />
-                    </div>
-                    <div className="row">
-                        <input type="password" className="form-input full-width" placeholder="Password"
-                            value={this.state.password}
-                            onChange={(e) => this.setState({ password: e.target.value })} />
-                    </div>
-                    <div className="row">
-                        <label className="err-msg">{this.props.errMsg}</label>
-                    </div>
-                    <div className="footer">
-                        <button className="login"
-                            onClick={e => {e.preventDefault(); this.props.onAction(this.state)}}>Login</button>
-                    </div>
-                </form>
-            </div>
-        )
-    }
-}
-
+                    </form>
+                </div>
+            )}
+        </Formik>
+    );
+};
 export default LoginForm;
